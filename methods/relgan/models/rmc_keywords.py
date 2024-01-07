e47c53d525132c468da8dd244c87e4090ffd02ba
littletree-Y <1148400914@qq.com>
2024-01-07 23:02:34 +0800

    bug fix

--- methods/relgan/models/rmc_keywords.py
+++ methods/relgan/models/rmc_keywords.py
@@ -165,13 +165,16 @@ def generator(x_real, keywords_onehot, keywords_len, temperature, vocab_size, ba
      # Convert target_keywords_embedding to a RaggedTensor
     # target_keywords_embedding_ragged = tf.RaggedTensor.from_row_lengths(target_keywords_embedding, row_lengths=keywords_len)
 
-
+    generated_text_adv_embedding = tf.matmul(tf.reshape(gen_x_onehot_adv, [-1, vocab_size]), g_embeddings)  # (batch_size * seq_len) x emb_dim
 
     def compute_keyword_loss(args):
         generated_text_embedding_sample, target_keywords_embedding_sample, num_keywords_sample = args
 
         # Only consider the valid keywords (i.e., non-padding keywords)
         valid_keywords_embedding_sample = target_keywords_embedding_sample[:num_keywords_sample]
+
+        # Ensure valid_keywords_embedding_sample is always 2D
+        valid_keywords_embedding_sample = tf.expand_dims(valid_keywords_embedding_sample, -1)
 
         # Calculate the cosine similarity between each word in the generated text and each valid keyword
         similarity_per_word = cosine_similarity(generated_text_embedding_sample, valid_keywords_embedding_sample)  # seq_len x num_keywords_sample
@@ -186,21 +189,16 @@ def generator(x_real, keywords_onehot, keywords_len, temperature, vocab_size, ba
     # Reshape generated_text_embedding to batch_size x seq_len x emb_dim
     generated_text_embedding = tf.reshape(generated_text_embedding, [batch_size, seq_len, gen_emb_dim])
 
-    generated_text_adv = tf.matmul(tf.reshape(gen_x_onehot_adv, [-1, vocab_size]), g_embeddings)  # (batch_size * seq_len) x emb_dim
-
     # Reshape generated_text_adv_embedding to batch_size x seq_len x emb_dim
-    generated_text_adv_embedding = tf.reshape(generated_text_adv, [batch_size, seq_len, gen_emb_dim])
+    generated_text_adv_embedding = tf.reshape(generated_text_adv_embedding, [batch_size, seq_len, gen_emb_dim])
 
     # Calculate keyword loss for each sample in the batch
     pretrain_keyword_loss = tf.map_fn(compute_keyword_loss, elems=(generated_text_embedding, target_keywords_embedding, keywords_len), dtype=tf.float32)
     pretrain_keyword_loss = tf.reduce_mean(pretrain_keyword_loss)
 
-    
-
     # Calculate keyword loss for each sample in the batch for adversarial training
     adv_keyword_loss = tf.map_fn(compute_keyword_loss, elems=(generated_text_adv_embedding, target_keywords_embedding, keywords_len), dtype=tf.float32)
     adv_keyword_loss = tf.reduce_mean(adv_keyword_loss)
-
 
     return gen_x_onehot_adv, gen_x, pretrain_loss, gen_o, pretrain_keyword_loss, adv_keyword_loss
 
